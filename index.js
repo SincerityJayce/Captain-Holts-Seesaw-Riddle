c1273664e6781a2a5f6c6ed1c5e53967e4fb6217
SincerityJayce <84999232+SincerityJayce@users.noreply.github.com>
2024-04-13 17:31:33 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-import { CaptainHolts_islanderRiddle } from './CaptainHolts_islanderRiddle.js'
+import { CaptainHolts_islanderRiddle, islanders, seesaw } from './CaptainHolts_islanderRiddle.js'
 import { answerToHoltsIslanderRiddle } from './VincesAnswerToHoltsIslanderRiddle.js'
-export { CaptainHolts_islanderRiddle, answerToHoltsIslanderRiddle }
+export { CaptainHolts_islanderRiddle, islanders, seesaw, answerToHoltsIslanderRiddle }
 
 
 // CaptainHolts_islanderRiddle(answerToHoltsIslanderRiddle) 
